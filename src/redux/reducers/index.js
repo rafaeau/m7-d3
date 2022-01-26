962c36bbecf77342ff103f77c262806ddb871f11
Rafael Ferreira <rafaelnevesf@gmail.com>
2022-01-26 20:06:50 +0100

    DEL_FROM_FAVORITES

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -8,6 +8,12 @@ const mainReducer = (state = initialState, action) => {
                 ...state,
                 favorites: [...state.favorites, action.payload]
             }
+        
+        case 'DEL_FROM_FAVORITES':
+            return {
+                ...state,
+                favorites: state.favorites.filter((job, i) => i !== action.payload)
+            }
         default:
             return state
     }
